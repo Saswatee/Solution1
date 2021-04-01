c0f40ca72bcf093e92dfd0b018f45529dd339637
Saswatee <saswateedasguddy@gmail.com>
2021-04-01 12:43:17 +0530

    Initial Commit

---
+++ NGO.MyWeb/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NGO.MyWeb.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+    
+    public ActionResult Success()
+    {
+
+        return View();
+    }
+        public ActionResult Failure()
+        {
+
+            return View();
+
+        } 
+        
+       }
+}
